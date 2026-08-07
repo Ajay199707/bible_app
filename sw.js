90ca957cd736b02c52dca99511c9dffb77018857
Ajay199707 <Ajay199707@users.noreply.github.com>
2026-08-07 05:05:35 +0000

    deploy: 1d00c9d3ad8707df2d50fe5e940b80e714078a14

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'bible-app-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/data/english_bible.json',
+  '/data/tamil_bible.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).catch(() => caches.match('/index.html'));
+    })
+  );
+});
